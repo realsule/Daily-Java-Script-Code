c698cf39660ff70f8e521eec77188cb9018fa790
Real Sule <real1sule@gmail.com>
2026-08-16 03:45:30 +0300

    my first buttons practice

---
+++ Buttons/script.js
@@ -0,0 +1,21 @@
+  document.getElementById('pri-btn').addEventListener('click', function() {
+    // Option A: Alert
+    alert('Button is working!');
+  });
+/*
+document.getElementById('pri-btn').addEventListener('click', function() {
+  // Option B: Change text
+  this.textContent = 'Clicked!';
+}--makes the button text change to "Clicked!" when clicked
+*/
+
+
+document.getElementById('sec-btn').addEventListener('click', function() {
+  
+ alert('I love JavaScript!');
+});
+
+document.getElementById('ter-btn').addEventListener('click', function() {
+  
+ alert('I love Coding!');
+});
